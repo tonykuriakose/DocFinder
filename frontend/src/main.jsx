68afd9bb4762e3ec47cca39e05abafabadfcabad
Tony <t4tonykuriakose@gmail.com>
2025-01-18 10:51:43 +0530

    Speciality Menu

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import AppContextProvider from './context/AppContext.jsx'
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </BrowserRouter>,
+)
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
+import SpacialityMenu from "../components/SpacialityMenu";
+import TopDoctors from "../components/TopDoctors";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <SpacialityMenu />
+      <TopDoctors />
       
     </div>
   );
