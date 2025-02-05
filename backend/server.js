6793fe68a9a3b50ebd960cc3fbebe5f6209450bb
Tony <t4tonykuriakose@gmail.com>
2025-02-05 21:21:48 +0530

    cloudinary

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from "./config/mongodb.js"
+import connectCloudinary from "./config/cloudinary.js"
 
 
 const app = express()
@@ -11,7 +12,7 @@ connectDB()
 
 app.use(express.json())
 app.use(cors())
-
+connectCloudinary();
 
 app.get("/", (req, res) => {
   res.send("API Working")
