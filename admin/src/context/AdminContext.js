62d08535b3b6ca9eea616e895d3d3baa37dad9d1
Tony <t4tonykuriakose@gmail.com>
2025-02-06 08:01:47 +0530

    context

---
+++ admin/src/context/AdminContext.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { createContext, useState } from "react";
+import { toast } from "react-toastify";
+
+
+export const AdminContext = createContext()
+
+const AdminContextProvider = (props) => {
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
+
+    const [appointments, setAppointments] = useState([])
+    const [doctors, setDoctors] = useState([])
+    const [dashData, setDashData] = useState(false)
+
+    // Getting all Doctors data from Database using API
+    const getAllDoctors = async () => {
+
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/admin/all-doctors', { headers: { aToken } })
+            if (data.success) {
+                setDoctors(data.doctors)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+
+    }
+}
+
+
+    export default AdminContextProvider;
---
+++ admin/src/context/DoctorContext.js
@@ -0,0 +1,36 @@
+import { createContext, useState } from "react";
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
+
+export const DoctorContext = createContext()
+
+const DoctorContextProvider = (props) => {
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const [dToken, setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : '')
+    const [appointments, setAppointments] = useState([])
+    const [dashData, setDashData] = useState(false)
+    const [profileData, setProfileData] = useState(false)
+    const getAppointments = async () => {
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/doctor/appointments', { headers: { dToken } })
+
+            if (data.success) {
+                setAppointments(data.appointments.reverse())
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+}
+
+
+
+    export default DoctorContextProvider;
