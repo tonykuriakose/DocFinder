a41b0186ad42ce452e126454c28717fa273b40fb
Tony <t4tonykuriakose@gmail.com>
2025-02-06 20:13:25 +0530

    user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,57 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import validator from "validator";
+import userModel from "../models/userModel.js";
+import doctorModel from "../models/doctorModel.js";
+import appointmentModel from "../models/appointmentModel.js";
+import { v2 as cloudinary } from 'cloudinary'
+import stripe from "stripe";
+import razorpay from 'razorpay';
+
+
+
+
+
+
+// API to register user
+const registerUser = async (req, res) => {
+
+    try {
+        const { name, email, password } = req.body;
+
+        // checking for all data to register user
+        if (!name || !email || !password) {
+            return res.json({ success: false, message: 'Missing Details' })
+        }
+
+        // validating email format
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Please enter a valid email" })
+        }
+
+        // validating strong password
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Please enter a strong password" })
+        }
+
+        // hashing user password
+        const salt = await bcrypt.genSalt(10); // the more no. round the more time it will take
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const userData = {
+            name,
+            email,
+            password: hashedPassword,
+        }
+
+        const newUser = new userModel(userData)
+        const user = await newUser.save()
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+
+        res.json({ success: true, token })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+} 
