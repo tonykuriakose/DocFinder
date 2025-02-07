84bd4bf18ee4ce9e28686ec8d5de808a6171a66d
Tony <t4tonykuriakose@gmail.com>
2025-02-07 06:27:20 +0530

    appointment

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,17 @@
 import express from 'express';
-import { loginUser, registerUser,getProfile} from '../controllers/userController.js';
+import { loginUser, registerUser,getProfile,updateProfile,bookAppointment} from '../controllers/userController.js';
+import upload from '../middleware/multer.js';
+import authUser from '../middleware/authUser.js';
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
-userRouter.get("/get-profile", authUser, getProfile)
+userRouter.get("/get-profile", authUser, getProfile);
+userRouter.post("/update-profile",upload.single('image',authUser,updateProfile));
+userRouter.post("/book-appointment", authUser, bookAppointment);
+
+
+
+
+
 export default userRouter;
