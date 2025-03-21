eb2258e642befe2807801b8a3f8b6b35c437717e
Tony <t4tonykuriakose@gmail.com>
2025-03-21 09:59:43 +0530

    deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import AppContextProvider from './context/AppContext.jsx'
 
+// building
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AppContextProvider>
